c276f02672893d5fbc08e25b0a77d4d4c0be39f6
rsemkov <136321984+rsemkov@users.noreply.github.com>
2024-01-11 18:07:41 +0200

    Added word search functionality

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import re
 
 
-def document_stats(file_content):
+def option_1_document_stats(file_content):
     def count_words_and_avg_word_len():
         words_pattern = r'\b\w+\b'
 
@@ -42,6 +42,55 @@ def document_stats(file_content):
            f"\n-Total sentences in the file: {sent_count}"
 
 
+def option_2_word_search(file_content, searched_word):
+    coordinates_result = ""
+
+    def word_occurrences_count():
+        words_pattern = r'\b\w+\b'
+        # find all words:
+        words_only = re.findall(words_pattern, file_content)
+
+        # count searched word occurrences:
+        word_occurrences = sum([1 for word in words_only if word.lower() == searched_word.lower()])
+
+        return word_occurrences
+
+    def word_coordinates_search():
+        coordinates = []
+        data_matrix = []
+
+        # SPLITS THE TEXT BY ROWS
+        data = file_content.split('\n')
+
+        # REMOVES WHITE SPACES, AND ALL CHARS WHICH ARE NOT ALPHABETIC(ONLY ACTUAL WORDS REMAIN), ADDS THEM TO MATRIX
+        for current_row in data:
+            current_row = ''.join(current_row).split()
+            for index in range(len(current_row)):
+                current_row[index] = "".join(x for x in current_row[index] if x.isalpha())
+            data_matrix.append(current_row)
+
+        # SEARCHES FOR THE WORD IN THE MATRIX AND ADDS ALL COORDINATES TO THE LIST, AS TUPLE
+        for i, row in enumerate(data_matrix):
+            for j, element in enumerate(row):
+                if element.lower() == searched_word.lower():
+                    coordinates.append((i, j))
+
+        return coordinates
+
+    word_count = word_occurrences_count()
+    word_coordinates = word_coordinates_search()
+
+    for r, c in word_coordinates:
+        coordinates_result += f"\nRow: {r + 1}, Word number: {c + 1}"
+
+    if word_count == 0:
+        return f"\nThe word '{searched_word}' cannot be found in the document."
+    else:
+        return f"\nThe word '{searched_word}' can be found {word_count} time/s in the document." \
+               f"\nIt can be found at the following coordinates:" \
+               f"{coordinates_result}"
+
+
 def options_display_menu():
     print("\nOptions Menu:")
     print("1. Documents stats")
@@ -66,10 +115,12 @@ def main():
         option_selected = input("\nPlease, enter your choice from the menu: ")
 
         if option_selected == "1":
-            print(document_stats(content))
+            print(option_1_document_stats(content))
             break
         elif option_selected == "2":
-            pass
+            word_searched = input("\nPlease, enter the word you would like to search for(case INSENSITIVE): ")
+            print(option_2_word_search(content, word_searched))
+            break
         elif option_selected == "3":
             print("\nThank you for using our service. Goodbye!")
             break
